e3fdb7506ff34e2987e8ce0045f5a976a7c60c1e
DAGHER Maroun <maroun.dagher@wfp.org>
2018-05-29 17:48:23 +0300

    first commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\DataService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class DataController extends Controller
+{
+	public function __construct(DataService $dataService, Request $request)
+	{
+		$this->request = $request;
+		$this->dataService = $dataService;
+	}
+
+	public function getAvailableDatabases()
+	{
+		return $this->dataService->getDatabases();
+	}
+
+    public function getData()
+    {
+    	return $this->dataService->getData();
+    }
+
+    public function getDataWithShared1()
+    {
+    	$data = $this->getData();
+    	$collection = $data->where('shared', 1);
+
+    	// return data in pages to display
+    	return $this->dataService->paginate($collection);
+    }
+
+    public function getDataWithShared0()
+    {
+    	$data = $this->getData();
+    	$collection = $data->where('shared', 0);
+
+    	// return data in pages to display
+    	return $this->dataService->paginate($collection);
+    }
+
+    public function getDataWithCreator1()
+    {
+    	$data = $this->getData();
+    	$collection = $data->where('creator', 1);
+
+    	// return data in pages to display
+    	return $this->dataService->paginate($collection);
+    }
+
+    public function getDataWithCreator0()
+    {
+    	$data = $this->getData();
+    	$collection = $data->where('creator', 0);
+
+    	// return data in pages to display
+    	return $this->dataService->paginate($collection);
+    }
+
+    public function findDuplicatedFileNames()
+    {
+    	$data = $this->getData();
+    	$collection_unique = $data->unique('filename');
+
+	    $collection_grouped = $data->groupBy('filename');
+
+	    $dupes = $collection_grouped->filter(function (Collection $groups) {
+	        return $groups->count() > 1;
+	    });
+
+	    $file_names_dups = array();
+
+	    foreach ($dupes as $key => $value) {
+	    	array_push($file_names_dups, $key);
+	    }
+
+		// $duplicated_name = $data->diff($collection_unique);
+    	// return data in pages to display
+    	return $file_names_dups;
+    }
+
+    public function findByDate()
+    {
+    	$start_date = isset($this->request->start_date) ? Carbon::parse($this->request->start_date) : Carbon::parse('2018-01-01');
+    	$end_date = isset($this->request->end_date) ? Carbon::parse($this->request->end_date) : Carbon::now();
+    	$data = $this->getData();
+    	
+    	$start_date = new \DateTime($start_date);
+    	$end_date = new \DateTime($end_date);
+
+    	$collection = $data->where('date_modified', '>=', $start_date->getTimestamp())->where('date_modified', '<=', $end_date->getTimestamp());
+
+		return $this->dataService->paginate($collection);
+    }
+}
---
+++ app/Services/DataService.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Services;
+
+use DB;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Config;
+
+class DataService {
+	public function __construct(Request $request)
+	{
+		$this->request = $request;
+	}
+
+	public function getDatabases()
+	{
+    	$system_databases = ['information_schema', 'DALILI', 'VOICES', 'WFP', 'homestead', 'livelyhoods', 'mysql', 'performance_schema', 'sys'];
+		
+		$link = mysqli_connect(
+			env('DB_HOST', '127.0.0.1'), 
+			env('DB_USERNAME', 'homestead'), 
+			env('DB_PASSWORD', '')
+		);
+
+		$res = mysqli_query($link, "SHOW DATABASES");
+
+		$databases = array();
+
+		while ($row = mysqli_fetch_assoc($res)) {
+			if(!in_array($row['Database'], $system_databases)) {
+		    	array_push($databases, $row);
+			}
+		}
+
+		return $databases;
+	}
+
+	public function getTablesInDB($db)
+	{
+		DB::purge('mysql');
+
+		Config::set("database.connections.mysql", [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => $db,
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null
+		]);
+
+    	$tables = DB::select('SHOW TABLES');
+
+    	$data = array();
+
+    	foreach ($tables as $key => $table) {
+    		$table_name = "Tables_in_".$db;
+    		array_push($data, $table->$table_name);
+    	}
+
+    	return $data;
+
+	}
+
+    public function getDataFromDB($tables)
+    {
+		$data = array();
+
+		$old_collection = null;
+
+		foreach ($tables as $key => $table) {
+			$next_collection = DB::table($table)->select('*')->get();
+			$merged_collection = $next_collection->merge($old_collection);
+			$old_collection = $next_collection;
+		}
+
+		return $merged_collection;
+    }
+
+    public function getData()
+    {
+    	$databases = $this->getDatabases();
+
+    	$test = [];
+
+    	foreach ($databases as $key => $database) {
+    		$tables_in_db = $this->getTablesInDB($database['Database']);
+    		$data_in_db[$database['Database']] = $this->getDataFromDB($tables_in_db);
+    		// $data_in_db[$database['Database']] = $this->getDataFromDB($tables_in_db);
+    	}
+
+    	$main_collection = null;
+
+    	foreach ($data_in_db as $database => $collection) {
+    		$main_collection = $collection->merge($main_collection);
+    	}
+
+    	return $main_collection;
+    }
+
+    public function paginate($collection)
+    {
+		$page = isset($this->request->page) ? $this->request->page : 1;
+		$perPage = isset($this->request->perPage) ? $this->request->perPage : 100;
+		$offset = ($page * $perPage) - $perPage;
+
+		return new LengthAwarePaginator(
+		    array_slice($collection->toArray(), $offset, $perPage, true), // Only grab the items we need
+		    count($collection), // Total items
+		    $perPage, // Items per page
+		    $page, // Current page
+		    ['path' => $this->request->url(), 'query' => $this->request->query()]
+		);
+    }
+
+    public function findDuplicate($field)
+    {
+    	
+    }
+}
